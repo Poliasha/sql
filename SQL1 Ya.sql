0fdb3f7e011d9944783d5242bd01380644f7aa46
9161-lebedevalr <lubov.lebedeva@mail.ru>
2016-07-05 22:24:24 +0400

    2 commit

--- SQL1 Ya.sql
+++ SQL1 Ya.sql
@@ -7,7 +7,7 @@ nvl((SELECT sum(p.amount) AS montn_amount
         WHERE p.student_id = s.id AND to_char(p.payment_date,'yyyy.mm') = to_char(add_months(SYSDATE,-1),'yyyy.mm')), 0) AS prior_month_sum
 FROM Student s
 )
-WHERE prior_month_sum < 1500 
+WHERE prior_month_sum < 1500  
 
 
 
--- SQL2 Ya.sql
+++ SQL2 Ya.sql
@@ -9,7 +9,7 @@ SELECT s.id, s.name, s.surname,
 nvl((select sum(p.amount) as montn_amount from Payment p where p.student_id = s.id and to_char(p.payment_date,'yyyy.mm') = to_char(add_months(SYSDATE,-1),'yyyy.mm')), 0) as prior_month_sum
 FROM Student s
 )
-where prior_month_sum < 1500
+where prior_month_sum < 1500 
 )
 where rn <= 3  
 
